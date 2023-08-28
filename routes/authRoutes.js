186f2d91364edca4e7791a07bff0aeb894fc90b3
Alvaro Munoz <alvarogab5@gmail.com>
2023-08-28 20:54:47 -0300

    agregue logout

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,8 @@ const {
     signup_get,
     signup_post,
     login_get,
-    login_post
+    login_post,
+    logout_get
 } = require("../controllers/authController");
 
 const router = Router();
@@ -15,7 +16,7 @@ router.get('/signup', signup_get);
 router.post('/signup', signup_post);
 router.get('/login', login_get);
 router.post('/login', login_post);
-// router.get('/logout', ()=>{});
+router.get('/logout', logout_get);
 
 
 module.exports = router;
