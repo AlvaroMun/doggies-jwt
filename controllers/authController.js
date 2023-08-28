2ac2634fd0497d73036c82c8c488e269afc206bb
Alvaro Munoz <alvarogab5@gmail.com>
2023-08-28 19:46:35 -0300

    agregue mensaje de error en singup

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,44 @@ const createToken = (id) => {
     })
 }
 
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let errors = {
+        email: '',
+        password: ''
+    };
+
+    // incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'That email is not registered';
+    }
+
+    // incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'That password is incorrect';
+    }
+
+    // duplicate email error
+    if (err.code === 11000) {
+        errors.email = 'that email is already registered';
+        return errors;
+    }
+
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+        // console.log(err);
+        Object.values(err.errors).forEach(({
+            properties
+        }) => {
+            // console.log(val);
+            // console.log(properties);
+            errors[properties.path] = properties.message;
+        });
+    }
+
+    return errors;
+}
+
 const signup_get = (req, res) => {
     res.render('singup')
 };
@@ -41,7 +79,12 @@ const signup_post = async (req, res) => {
             user: user._id
         })
     } catch (error) {
-        res.status(400).send(error.message)
+        console.log({
+            errors: handleErrors(error)
+        });
+        res.status(400).json({
+            errors: handleErrors(error)
+        })
     }
 }
 const login_post = async (req, res) => {
